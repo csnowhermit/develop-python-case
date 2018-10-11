6751813ebcbf0743f15b2f8079ea922dfbc35624
RxtBD <920164255@qq.com>
2018-10-11 14:37:24 +0800

    add tailF.py

--- demo/timeDemo.py
+++ demo/timeDemo.py
@@ -2,3 +2,15 @@
 import time
 
 print(time.strftime("%Y%m%d_%H%M%S", time.localtime()))
+
+'''
+    获取n秒后的时间，n为负数则为获取n秒前的时间
+'''
+def getSpecifyTime(n, fmt):
+    now = time.time()
+    before = now + n
+
+    return time.strftime(fmt, time.localtime(before))
+
+print(getSpecifyTime(-86400, "%Y%m%d_%H%M%S"))
+print(getSpecifyTime(-86400, "%Y%m%d"))
--- jijin/shares/getStockInfo_V2.0.py
+++ jijin/shares/getStockInfo_V2.0.py
@@ -2,7 +2,6 @@
 
 import requests
 from bs4 import BeautifulSoup
-import traceback
 import re
 import time
 import sys
