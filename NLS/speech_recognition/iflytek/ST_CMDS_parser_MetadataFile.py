40ccecfebe4cb35fc771b393dc1d5dc08d560fb9
RxtBD <920164255@qq.com>
2019-10-24 11:02:30 +0800

    add: ST_CMDS_20170001_1-OS數據集，.metadata文件解析脚本

---
+++ NLS/speech_recognition/iflytek/ST_CMDS_parser_MetadataFile.py
@@ -0,0 +1,41 @@
+import os
+
+'''
+    预处理ST-CMDS-20170001_1-OS数据：.metadata文件解析
+'''
+
+st_path = "D:/workspace/openSourceModel/ASRT_SpeechRecognition/dataset/ST-CMDS-20170001_1-OS/"
+
+details = open("D:/data/iat_tingxie/metadata.txt", 'w', encoding='utf-8')
+text = open("D:/data/iat_tingxie/text.txt", 'w', encoding='utf-8')
+
+for file in os.listdir(st_path):
+    if file.endswith(".metadata"):
+        content = ""
+        try:
+            with open(st_path + file, 'r', encoding='utf-8') as fo:
+                for line in fo.readlines():
+                    line = line.rstrip("\n")
+                    if line.__contains__("LBN"):
+                        content = content + line + ","
+                    elif line.__contains__("SEX"):
+                        content = content + line + ","
+                    elif line.__contains__("AGE"):
+                        content = content + line + ","
+                    elif line.__contains__("ACT"):
+                        content = content + line + ","
+                    elif line.__contains__("BIR"):
+                        content = content + line
+        except Exception:
+            pass
+        print(content)
+        details.write(content + "\n")
+    elif file.endswith(".txt"):
+        try:
+            with open(st_path + file, 'r', encoding='utf-8') as fo:
+                line = fo.readline()
+                text.write(file + " " + line + "\n")
+        except Exception:
+            pass
+    else:
+        pass
