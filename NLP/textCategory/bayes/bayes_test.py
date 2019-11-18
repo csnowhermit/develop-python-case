9c0b7ed9777a33dd1f5cca2a04418f78ff042cf9
RxtBD <920164255@qq.com>
2019-11-18 19:02:07 +0800

    update: 贝叶斯文本分类

---
+++ NLP/textCategory/bayes/bayes_test.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+
+import os
+from sklearn.externals import joblib
+from NLP.textCategory.bayes.bayes_train import get_dataset, split_train_and_test_set, multinamialNB_save_path, bernousNB_save_path
+
+'''
+    从文件读取模型并进行预测
+'''
+
+test_data = get_dataset()
+train_set_tmp, train_label_tmp, test_set, test_label = split_train_and_test_set(test_data, 0.0)
+
+'''
+    获取最新的模型
+'''
+def get_newest_model(model_path):
+    if os.path.exists(model_path):
+        # 按文件最后修改时间排序，reverse=True表示降序排序
+        filelist = sorted(os.listdir(model_path), key=lambda x: os.path.getctime(os.path.join(model_path, x)), reverse=True)
+        return os.path.join(model_path, filelist[0])
+
+
+
+'''
+    测试多项式分类器
+'''
+def test_bayes(model_file):
+    clf_multi = joblib.load(model_file)
+    predict = clf_multi.predict(test_set)
+
+    count = 0
+    for left, right, tset in zip(predict, test_label, test_set):
+        print(left, "-->", right, "-->", tset)
+        if left == right:
+            count += 1
+    print(model_file, "准确率：", count / len(test_label))
+
+
+
+def main():
+    test_bayes(get_newest_model(multinamialNB_save_path))
+    test_bayes(get_newest_model(bernousNB_save_path))
+    print(get_newest_model(multinamialNB_save_path))
+    print(get_newest_model(bernousNB_save_path))
+
+if __name__ == '__main__':
+    main()
--- NLP/textCategory/bayes/bayes_train.py
+++ NLP/textCategory/bayes/bayes_train.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import os
+import time
 import random
 import jieba
 from sklearn.naive_bayes import MultinomialNB
@@ -7,12 +9,22 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer, CountVectorizer
 from sklearn import metrics
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.externals import joblib
 
 '''
     贝叶斯：文本分类
 '''
 
 jieba.load_userdict("../atomic.txt")    # 不可切分词
+multinamialNB_save_path = "./model/multinamialNB/"
+bernousNB_save_path = "./model/bernousNB/"
+
+if os.path.exists(multinamialNB_save_path) is False:
+    os.mkdir(multinamialNB_save_path)
+if os.path.exists(bernousNB_save_path) is False:
+    os.mkdir(bernousNB_save_path)
+
+
 
 '''
     加载数据集：最原始文本集
@@ -68,6 +80,7 @@ def getAllKeywords(zhuhai_c, others):
     return set(keywords)
 
 keywords = getAllKeywords(zhuhai_c, others)
+stopwords = getWordList("../stopwords.txt")
 
 
 '''
@@ -85,7 +98,7 @@ def get_words(line):
             a = a
         else:
             a = ""
-        if len(a)>0:
+        if len(a) > 0:
             s = s + a + " "
     return s.strip(" ")
 
@@ -93,8 +106,8 @@ def get_words(line):
 '''
     划分训练集和测试集
 '''
-def split_train_and_test_set(data):
-    filesize = int(0.7 * len(data))    # 训练集:测试集=7:3
+def split_train_and_test_set(data, rate):
+    filesize = int(rate * len(data))    # 训练集:测试集=7:3
 
     train_set = [each[0] for each in data[:filesize]]
     train_label = [each[1] for each in data[:filesize]]
@@ -118,10 +131,11 @@ def multinamialNB(train_set, train_label, test_set, test_label):
     predict = nbc.predict(test_set)    # 测试分类器分类效果
     count = 0
     for left, right, tset in zip(predict, test_label, test_set):
-        # print(left, "-->", right, "-->", tset)
+        print(left, "-->", right, "-->", tset)
         if left == right:
             count += 1
     print("多项式分类器准确率：", count/len(test_label))
+    joblib.dump(nbc, multinamialNB_save_path + "multinamialNB_" + str(int(time.time())) + ".m")
 
 '''
     伯努利分类器
@@ -136,24 +150,55 @@ def bernousNB(train_set, train_label, test_set, test_label):
     nbc_1.fit(train_set, train_label)
     predict = nbc_1.predict(test_set)  # 在测试集上预测结果
     count = 0  # 统计预测正确的结果个数
-    for left, right in zip(predict, test_label):
+    for left, right, tset in zip(predict, test_label, test_set):
+        print(left, "-->", right, "-->", tset)
         if left == right:
             count += 1
+        else:
+            # 预测错的，单独记录
+            if str(tset).__contains__("这个"):    # 如果存在“这个”关键字时，需进一步判断
+                pass
     print("伯努利分类器准确率：", count / len(test_label))
+    joblib.dump(nbc_1, "bernousNB.m")
+    joblib.dump(nbc_1, bernousNB_save_path + "bernousNB_" + str(int(time.time())) + ".m")
 
 
 
 
 def main():
-    for i in range(100):    # 多轮训练
-        data = get_dataset()    # 加载最原始的输入数据
+    # for i in range(100):    # 多轮训练
+        # # data = get_dataset()    # 加载最原始的输入数据
         # data = load_dataset()  # 加载手动提取的关键词数据
-        train_set, train_label, test_set, test_label = split_train_and_test_set(data)
-        print("第", i, "轮：")
-        multinamialNB(train_set, train_label, test_set, test_label)
-        bernousNB(train_set, train_label, test_set, test_label)
+        # train_set, train_label, test_set, test_label = split_train_and_test_set(data, 1.0)
+        # # print(train_set)
+        # # print(train_label)
+        # # print(test_set)
+        # # print(test_label)
+        # print("第", i, "轮：")
+        # test_set = train_set
+        # test_label = train_label
+        # multinamialNB(train_set, train_label, test_set, test_label)
+        # bernousNB(train_set, train_label, test_set, test_label)
 
     ## 新想法：全部数据用来训练，新截取关键词用来测试
+    # 1.加载准备好的关键词及意图，作为训练数据集
+    org_data = load_dataset()
+    train_set, train_label, test_set_tmp, test_label_tmp = split_train_and_test_set(org_data, 1.0)
+
+    # 2.加载原始数据，现场切词，作为测试数据集
+    test_data = get_dataset()
+    train_set_tmp, train_label_tmp, test_set, test_label = split_train_and_test_set(test_data, 0.0)
+
+    # for left, right in zip(train_set, train_label):
+    #     print(left, "-->", right)
+
+    # for left, right in zip(test_set, test_label):
+    #     print(left, "==>", right)
+
+    # 3.训练模型并得出准确率
+    multinamialNB(train_set, train_label, test_set, test_label)
+    bernousNB(train_set, train_label, test_set, test_label)
+
 
 if __name__ == '__main__':
     main()
