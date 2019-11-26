dd33e5342d482b58f19eef4db22e3979485795c8
RxtBD <920164255@qq.com>
2019-11-26 17:19:35 +0800

    重構代碼

---
+++ NLS/speech_recognition/ASR/rtasr_webapi.py
@@ -0,0 +1,115 @@
+#-*- encoding:utf-8 -*-
+
+import sys
+import hashlib
+from hashlib import sha1
+import hmac
+import base64
+from socket import *
+import json, time, threading
+from websocket import create_connection
+import websocket
+from urllib.parse import quote
+import logging
+import pyaudio
+
+'''
+    读取pcm文件进行实时语音转写
+'''
+
+logging.basicConfig()
+
+base_url = "wss://rtasr.xfyun.cn/v1/ws"
+# app_id = "5d760a37"
+# api_key = "e5aba0511eea04e2ab986cdf1e4b6972"
+app_id = "5dc4b509"
+api_key = "155b5599042f7187727214ba5af7b3ef"
+file_path = "../iflytek/静夜思_李白.pcm"
+# file_path = "D:/data/iat_tingxie/157242600644468.wav"
+
+end_tag = "{\"end\": true}"
+
+CHUNK = 256
+FORMAT = pyaudio.paInt16
+CHANNELS = 1  # 声道数
+RATE = 16000  # 采样率
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+index = 1
+
+class Client():
+    def __init__(self):
+        # 生成鉴权参数，公式：HmacSHA1(MD5(appid + ts), api_key)
+        ts = str(int (time.time()))
+        tmp = app_id + ts
+        hl = hashlib.md5()    # h1.hexdigest()，返回16进制
+        hl.update(tmp.encode(encoding='utf-8'))    # MD5(appid + ts)
+        # my_sign = hmac.new(api_key, hl.hexdigest(), sha1).digest()
+        my_sign = hmac.new(bytes(api_key, 'utf-8'), bytes(hl.hexdigest(), 'utf-8'), digestmod='sha1').digest()
+        signa = base64.b64encode(my_sign)
+
+        self.ws = create_connection(base_url + "?appid=" + app_id + "&ts=" + ts + "&signa=" + quote(signa))
+        self.trecv = threading.Thread(target=self.recv)
+        self.trecv.start()
+
+    def send(self):
+        # file_object = open(file_path, 'rb')
+        try:
+            while True:
+                # chunk = file_object.read(1280)    # 实施转写问题，可以实时往pcm中写入
+                chunk = stream.read(640)
+                # print(type(chunk), len(chunk), index)
+                if not chunk:      # 如果当前没读到，说明暂时没人问，continue
+                    continue
+                # self.ws.send(chunk)      # 分批发送识别
+
+                global index
+                index += 1
+                time.sleep(0.04)
+        finally:
+            # print str(index) + ", read len:" + str(len(chunk)) + ", file tell:" + str(file_object.tell())
+            # file_object.close()
+            pass
+
+        # self.ws.send(bytes(end_tag, encoding='utf-8'))
+        print("send end tag success")
+
+    def recv(self):
+        try:
+            while self.ws.connected:
+                result = str(self.ws.recv())
+                if len(result) == 0:
+                    print("receive result end")
+                    break
+                result_dict = json.loads(result)
+
+                # 解析结果
+                if result_dict["action"] == "started":
+                    print("handshake success, result: " + result)
+
+                if result_dict["action"] == "result":
+                    print("rtasr result: " + result)
+
+                if result_dict["action"] == "error":
+                    print("rtasr error: " + result)
+                    self.ws.close()
+                    return
+        except websocket.WebSocketConnectionClosedException:
+            print("receive result end")
+
+    def close(self):
+        self.ws.close()
+        print("connection closed")
+
+
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.send()
---
+++ NLS/speech_recognition/IAT_SDK/tingxie_realtime_sdk.py
@@ -0,0 +1,131 @@
+# -*- coding：utf-8 -*-
+
+
+from ctypes import *
+import time
+import pyaudio
+import win32com.client
+
+FRAME_LEN = 640  # Byte
+MSP_SUCCESS = 0
+# 返回结果状态
+MSP_AUDIO_SAMPLE_FIRST = 1
+MSP_AUDIO_SAMPLE_CONTINUE = 2
+MSP_AUDIO_SAMPLE_LAST = 4
+MSP_REC_STATUS_COMPLETE = 5
+
+# 调用动态链接库
+dll = cdll.LoadLibrary("./windows_sdk/msc_x64.dll")
+# 登录参数，apppid一定要和你的下载SDK对应
+login_params = b"appid = 5d760a37, work_dir = ."
+
+# 实时录音
+CHUNK = 256
+FORMAT = pyaudio.paInt16
+CHANNELS = 1  # 声道数
+RATE = 16000  # 采样率
+RECORD_SECONDS = 60
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
+
+
+class Msp:
+    def __init__(self):
+        pass
+
+    def login(self):
+        ret = dll.MSPLogin(None, None, login_params)
+        # print('MSPLogin =>', ret)
+
+    def logout(self):
+        ret = dll.MSPLogout()
+        # print('MSPLogout =>', ret)
+
+    def isr(self, session_begin_params):
+        ret = c_int()
+        sessionID = c_voidp()
+        dll.QISRSessionBegin.restype = c_char_p
+        sessionID = dll.QISRSessionBegin(None, session_begin_params, byref(ret))
+        #print('QISRSessionBegin => sessionID:', sessionID, '\nret:', ret.value)
+
+        # 每秒【1000ms】  16000 次 * 16 bit 【20B】 ，每毫秒：1.6 * 16bit 【1.6*2B】 = 32Byte
+        # 1帧音频20ms【640B】 每次写入 10帧=200ms 【6400B】
+
+        # piceLne = FRAME_LEN * 20
+        piceLne = 1638 * 2
+        epStatus = c_int(0)
+        recogStatus = c_int(0)
+
+        frameSize = 8000  # 每一帧的音频大小
+        while True:
+            print(">")
+            wavData = stream.read(int(frameSize/4))
+            ret = dll.QISRAudioWrite(sessionID, wavData, len(wavData), MSP_AUDIO_SAMPLE_FIRST, byref(epStatus),
+                                     byref(recogStatus))
+            # print('len(wavData):', len(wavData), '\nQISRAudioWrite ret:', ret,'\nepStatus:', epStatus.value, '\nrecogStatus:', recogStatus.value)
+
+            time.sleep(0.1)
+            if len(wavData) == 0:    # 如果没采集到语音，跳过继续采集
+                continue
+            ret = dll.QISRAudioWrite(sessionID, wavData, len(wavData), MSP_AUDIO_SAMPLE_CONTINUE, byref(epStatus),
+                                     byref(recogStatus))
+            # print('len(wavData):', len(wavData), 'QISRAudioWrite ret:', ret, 'epStatus:', epStatus.value, 'recogStatus:', recogStatus.value)
+
+            # 添加语音结束标识
+            # ret = dll.QISRAudioWrite(sessionID, None, 0, MSP_AUDIO_SAMPLE_LAST, byref(epStatus), byref(recogStatus))
+            # print('len(wavData):', len(wavData), 'QISRAudioWrite ret:', ret, 'epStatus:', epStatus.value, 'recogStatus:', recogStatus.value)
+
+            print("\n所有待识别音频已全部发送完毕\n获取的识别结果:")
+
+            # -- 获取音频
+            laststr = ''
+            counter = 0
+            while recogStatus.value != MSP_REC_STATUS_COMPLETE:
+                ret = c_int()
+                dll.QISRGetResult.restype = c_char_p
+                retstr = dll.QISRGetResult(sessionID, byref(recogStatus), 0, byref(ret))
+                if retstr is not None:
+                    laststr += retstr.decode()
+                    print('333',laststr)
+                print('ret:', ret.value, 'recogStatus:', recogStatus.value)
+                counter += 1
+                time.sleep(0.2)
+                counter += 1
+                """
+              if counter == 50:
+                laststr += '讯飞语音识别失败'
+                break
+              """
+            print(laststr)
+
+        # 不知道为什么注解了？
+        #ret = dll.QISRSessionEnd(sessionID, '\0')
+
+
+        # print('end ret: ', ret)
+        # return laststr
+
+
+def XF_test(audio_rate):
+    msp = Msp()
+    #print("登录科大讯飞")
+    msp.login()
+    #print("科大讯飞登录成功")
+    session_begin_params = b"sub = iat, ptt = 0, result_encoding = utf8, result_type = plain, domain = iat"
+    if 16000 == audio_rate:
+        session_begin_params = b"sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = utf8"
+    msp.isr(session_begin_params)    # 该方法中循环采集语音，进行识别
+
+
+def main():
+    # 如果代码作为外置包被其他程序调用，请注释掉下两行；单独使用时保留
+    XF_test(16000)
+
+if __name__ == '__main__':
+    main()
