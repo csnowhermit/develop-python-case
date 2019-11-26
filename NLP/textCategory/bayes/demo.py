feb05e229364d7805921bdaa9eb5db9103345758
RxtBD <920164255@qq.com>
2019-11-26 16:04:42 +0800

    更新語料庫

--- NLP/textCategory/bayes/demo.py
+++ NLP/textCategory/bayes/demo.py
@@ -1,4 +1,13 @@
+# -*- coding:utf-8 -*-
 
-num = 1000
-print(1/1000)
-print(float(1/1000))
+import jieba
+import jieba.posseg as pesg
+
+jieba.load_userdict("../atomic.txt")
+with open("../原始例句.txt", encoding="utf-8") as fo:
+    for line in fo.readlines():
+        words = pesg.cut(line.split("\t")[0])
+        for w in words:
+            # print(w.flag, w.word, end="/ ")
+            print(w.flag, end=" ")
+        print()
