cbc70e794c425c3ce7d7b7d6569302fd91ced512
RxtBD <920164255@qq.com>
2019-12-02 17:06:31 +0800

    update: 更新模型

--- NLP/textCategory/bayes/demo.py
+++ NLP/textCategory/bayes/demo.py
@@ -2,12 +2,16 @@
 
 import jieba
 import jieba.posseg as pesg
+from NLP.textCategory.bayes.bayes_train import keywords, keywords_intention_file
 
-jieba.load_userdict("../atomic.txt")
-with open("../原始例句.txt", encoding="utf-8") as fo:
-    for line in fo.readlines():
-        words = pesg.cut(line.split("\t")[0])
-        for w in words:
-            # print(w.flag, w.word, end="/ ")
-            print(w.flag, end=" ")
-        print()
+# jieba.load_userdict("../atomic.txt")
+# with open("../原始例句.txt", encoding="utf-8") as fo:
+#     for line in fo.readlines():
+#         words = pesg.cut(line.split("\t")[0])
+#         for w in words:
+#             # print(w.flag, w.word, end="/ ")
+#             print(w.flag, end=" ")
+#         print()
+
+print(keywords_intention_file)
+print(keywords)
