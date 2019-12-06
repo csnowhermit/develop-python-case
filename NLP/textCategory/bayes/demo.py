6945e2252a6273539cb46212f9f71da0b0acea90
RxtBD <920164255@qq.com>
2019-12-06 21:01:49 +0800

    update： 更新訓練集

--- NLP/textCategory/bayes/demo.py
+++ NLP/textCategory/bayes/demo.py
@@ -17,7 +17,23 @@ from NLP.textCategory.bayes.bayes_train import keywords, keywords_intention_file
 # print(keywords_intention_file)
 # print(keywords)
 
-dirname = "D:/data/南站现场/广南一层中间12306录wav20191030/"
-for file in os.listdir(dirname):
-    if file.endswith(".old"):
-        os.rename(dirname + file, dirname + file[0: -4])
+# dirname = "D:/data/南站现场/广南一层中间12306录wav20191030/"
+# for file in os.listdir(dirname):
+#     if file.endswith(".old"):
+#         os.rename(dirname + file, dirname + file[0: -4])
+
+
+for i in range(1, 15):
+    print("南", i, "桥柱\t找南" ,i, "桥柱")
+    print("南", i, "桥\t找南" ,i, "桥柱")
+    print("南", i, "柱\t找南" ,i, "桥柱")
+    print("南", i, "桥墩\t找南" ,i, "桥柱")
+    print("南", i, "桥洞\t找南" ,i, "桥柱")
+    print("南", i, "洞口\t找南" ,i, "桥柱")
+
+list = []
+with open("../atomic.txt", encoding="utf-8") as fo:
+    for line in fo.readlines():
+        list.append(line.strip())
+
+print(len(set(list)))
