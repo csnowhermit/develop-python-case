0f0769a9e884398f77abb5c761e7724f22135d55
RxtBD <920164255@qq.com>
2020-02-03 15:36:54 +0800

    add: 图像对比

---
+++ CV/pictureCompare/PictureCompareDemo01.py
@@ -0,0 +1,41 @@
+from PIL import Image
+from PIL import ImageChops
+
+'''
+    图像对比
+'''
+
+def compare_images(path_one, path_two, diff_save_location):
+    """
+    比较图片，如果有不同则生成展示不同的图片
+
+    @参数一: path_one: 第一张图片的路径
+    @参数二: path_two: 第二张图片的路径
+    @参数三: diff_save_location: 不同图的保存路径
+    """
+    image_one = Image.open(path_one).convert('L')    # 打开并转为灰度图
+    image_two = Image.open(path_two).convert('L')
+    try:
+        diff = ImageChops.difference(image_one, image_two)
+
+        if diff.getbbox() is None:
+            # 图片间没有任何不同则直接退出
+            print("【+】We are the same!")
+        else:
+            diff.show(title="Compare")
+            diff.save(diff_save_location)
+    except ValueError as e:
+        text = ("表示图片大小和box对应的宽度不一致，参考API说明：Pastes another image into this image."
+                "The box argument is either a 2-tuple giving the upper left corner, a 4-tuple defining the left, upper, "
+                "right, and lower pixel coordinate, or None (same as (0, 0)). If a 4-tuple is given, the size of the pasted "
+                "image must match the size of the region.使用2纬的box避免上述问题")
+        print("【{0}】{1}".format(e, text))
+
+
+if __name__ == '__main__':
+    left = "D:/data/ss/gz03.jpg"
+    right = "D:/data/ss/gz03c.jpg"
+    result = "D:/data/ss/我们不一样.jpg"
+    compare_images(left,
+                   right,
+                   result)
---
+++ CV/pictureCompare/PictureCompareDemo02.py
@@ -0,0 +1,26 @@
+from PIL import Image
+import math
+import operator
+from functools import reduce
+
+'''
+    图像对比：计算图像的差异值
+    通过math.sqrt(直方图)的方式计算差异值
+'''
+
+def image_contrast(img1, img2):
+
+    image1 = Image.open(img1).convert('L')    # 打开并转为灰度图
+    image2 = Image.open(img2).convert('L')
+
+    h1 = image1.histogram()
+    h2 = image2.histogram()
+
+    result = math.sqrt(reduce(operator.add,  list(map(lambda a,b: (a-b)**2, h1, h2)))/len(h1) )
+    return result
+
+if __name__ == '__main__':
+    left = "D:/data/ss/gz03.jpg"
+    right = "D:/data/ss/gz03c.jpg"
+    result = image_contrast(left, right)
+    print(result)
