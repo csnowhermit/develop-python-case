4d04176a2cf86f4d58e846c3000da87cb0d428dc
RxtBD <920164255@qq.com>
2020-02-03 17:13:46 +0800

    update: 图像对比流程

--- CV/pictureCompare/PictureCompareDemo01.py
+++ CV/pictureCompare/PictureCompareDemo01.py
@@ -1,5 +1,8 @@
+import os
 from PIL import Image
 from PIL import ImageChops
+import cv2
+import numpy as np
 
 '''
     图像对比：输出两张图片的不同点到图片文件
@@ -31,11 +34,47 @@ def compare_images(path_one, path_two, diff_save_location):
                 "image must match the size of the region.使用2纬的box避免上述问题")
         print("【{0}】{1}".format(e, text))
 
+'''
+    grabcut方法进行图片语义分析，并返回结果图片
+'''
+def grabcutImageAndSave(source_path):
+    img = cv2.imread(source_path)
+    OLD_IMG = img.copy()
+    mask = np.zeros(img.shape[:2], np.uint8)
+    SIZE = (1, 65)
+    bgdModle = np.zeros(SIZE, np.float64)
+    fgdModle = np.zeros(SIZE, np.float64)
+    rect = (1, 1, img.shape[1], img.shape[0])
+    cv2.grabCut(img, mask, rect, bgdModle, fgdModle, 10, cv2.GC_INIT_WITH_RECT)
+
+    mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+    img *= mask2[:, :, np.newaxis]
+
+    path = source_path[0:source_path.rfind("/") + 1]    # 目录
+    filename = source_path[source_path.rfind("/") + 1: source_path.rfind(".")]    # 文件名
+    postfix = source_path[source_path.rfind("."):]    # 后缀
+    dest_path = os.path.join(path, filename + "_dest" + postfix)
+    print("new filename: ", dest_path)
+
+    cv2.imwrite(dest_path, img)    # 将处理过的图片保存至文件
+    return dest_path
 
 if __name__ == '__main__':
+    # 原图比较
     left = "D:/data/ss/gz03.jpg"
     right = "D:/data/ss/gz03c.jpg"
     result = "D:/data/ss/我们不一样.jpg"
-    compare_images(left,
-                   right,
-                   result)
+    # compare_images(left,
+    #                right,
+    #                result)    # 图片所有内容比较
+
+    left_dest = grabcutImageAndSave(left)
+    right_dest = grabcutImageAndSave(right)
+    compare_images(left_dest,
+                   right_dest,
+                   result)      # 图像分割后部分比较（降低不必比较部分的噪音的干扰）
+
+    from CV.pictureCompare.PictureCompareDemo02 import image_contrast
+
+    similarityDegree = image_contrast(left_dest, right_dest)    # 计算两图片的差异值
+    print("差异值", similarityDegree)
--- CV/semantic_segmentation/grabcut/grabcutDemo01.py
+++ CV/semantic_segmentation/grabcut/grabcutDemo01.py
@@ -1,13 +1,14 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
 '''
     grab cut方式进行图像语义分割
     分割之后的图：背景为黑色，其余地方不变
 '''
 
-img = cv2.imread('D:/data/ss/gz05.jpg')
+img = cv2.imread('D:/data/ss/gz03c.jpg')
 OLD_IMG = img.copy()
 mask = np.zeros(img.shape[:2], np.uint8)
 SIZE = (1, 65)
@@ -19,9 +20,12 @@ cv2.grabCut(img, mask, rect, bgdModle, fgdModle, 10, cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img *= mask2[:, :, np.newaxis]
 
-plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.title("grabcut"), plt.xticks([]), plt.yticks([])
-plt.subplot(122), plt.imshow(cv2.cvtColor(OLD_IMG, cv2.COLOR_BGR2RGB))
-plt.title("original"), plt.xticks([]), plt.yticks([])
+# plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+# plt.title("grabcut"), plt.xticks([]), plt.yticks([])
+# plt.subplot(122), plt.imshow(cv2.cvtColor(OLD_IMG, cv2.COLOR_BGR2RGB))
+# plt.title("original"), plt.xticks([]), plt.yticks([])
+#
+# plt.show()
 
-plt.show()
+plt.imshow(img), plt.colorbar(), plt.show()
+cv2.imwrite("D:/data/ss/gz03_new.jpg", img)
