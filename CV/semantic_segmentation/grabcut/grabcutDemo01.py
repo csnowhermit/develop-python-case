e5c022af9e6d6d80e53a6f5b7189da2865e230cc
RxtBD <920164255@qq.com>
2020-02-03 17:15:06 +0800

    update comment

--- CV/semantic_segmentation/grabcut/grabcutDemo01.py
+++ CV/semantic_segmentation/grabcut/grabcutDemo01.py
@@ -20,12 +20,13 @@ cv2.grabCut(img, mask, rect, bgdModle, fgdModle, 10, cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img *= mask2[:, :, np.newaxis]
 
-# plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-# plt.title("grabcut"), plt.xticks([]), plt.yticks([])
-# plt.subplot(122), plt.imshow(cv2.cvtColor(OLD_IMG, cv2.COLOR_BGR2RGB))
-# plt.title("original"), plt.xticks([]), plt.yticks([])
-#
-# plt.show()
+plt.subplot(121), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.title("grabcut"), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(cv2.cvtColor(OLD_IMG, cv2.COLOR_BGR2RGB))
+plt.title("original"), plt.xticks([]), plt.yticks([])
 
-plt.imshow(img), plt.colorbar(), plt.show()
-cv2.imwrite("D:/data/ss/gz03_new.jpg", img)
+plt.show()
+
+# # 只显示一个图形，并保存到文件
+# plt.imshow(img), plt.colorbar(), plt.show()
+# cv2.imwrite("D:/data/ss/gz03_new.jpg", img)
