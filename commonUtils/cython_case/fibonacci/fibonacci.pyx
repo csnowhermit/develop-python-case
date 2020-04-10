f503637c112b70d8782eba98507f355e8ac7d875
RxtBD <920164255@qq.com>
2020-04-10 15:00:17 +0800

    update: 新增，添加类型注释

--- commonUtils/cython_case/fibonacci/fibonacci.pyx
+++ commonUtils/cython_case/fibonacci/fibonacci.pyx
@@ -6,15 +6,20 @@ import time
     怎样调用？
         （1）编译Cython代码，并生成对应的动态链接库；
         （2）Python解释器载入动态链接库。
+
+    Note:
+        （1）@cython.boundscheck(False)和cython.wraparound(False)：关闭Cython的边界检查。
+        （2）cdef：定义函数, 并且可以给所有参数以及返回值指定类型。
+        （3）def：因为在python程序中, 我们是看不到cdef函数的, 所以我们这里要再用def定义一个fib函数, 来调用cdef的_fib函数。
 '''
 
-def fib(n):
+@cython.boundscheck(False)    # 关闭cython的边界检查
+@cython.wraparound(False)
+cdef int _fib(n):
     if not isinstance(n, int):
         raise ValueError('n is incorrect')
     if n <= 1: return n
     return fib(n-1) + fib(n-2)
 
-if __name__ == '__main__':
-    start = time.time()
-    print(fib(100))
-    print("耗时：", time.time() - start)
+def fib(n):
+    return _fib(n)
--- commonUtils/cython_case/matrixdot/matrixdot.pyx
+++ commonUtils/cython_case/matrixdot/matrixdot.pyx
@@ -1,11 +1,18 @@
 import time
 import numpy as np
+cimport cython
+cimport numpy as np    # 用来引入.pxd文件（相当于c/c++中的头文件）
 
 '''
     矩阵相乘
 '''
 
-def dot(m1, m2):
+@cython.boundscheck(False)    # 关闭cython的边界检查
+@cython.wraparound(False)
+cdef np.ndarray[np.float32_t, ndim=2] _dot(np.ndarray[np.float32_t, ndim=2] m1, np.ndarray[np.float32_t, ndim=2] m2):
+  cdef np.ndarray[np.float32_t, ndim=2] r    # 在函数内部, 我们可以使用cdef typename varname这样的语法来声明变量。
+  cdef int i, j, k
+  cdef np.float32_t s
   if m1.shape[1] != m2.shape[0]:
     raise ValueError('m1 and m2 dimension mismatch')
   r = np.zeros((m1.shape[0], m2.shape[1]), dtype=np.float32)
@@ -16,6 +23,9 @@ def dot(m1, m2):
         s += m1[i, k] * m2[k, j]
       r[i, j] = s
   return r
+
+def dot(m1, m2):
+  return _dot(m1, m2)
 
 if __name__ == '__main__':
     m1 = np.random.randn(100, 200)
